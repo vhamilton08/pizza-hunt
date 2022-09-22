1885792a68e390b49e23fd70a0dd176e69f64772
Vincent Hamilton <flunky9@protonmail.com>
2022-09-21 20:50:51 -0600

    added comment model and routes

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,23 +1,44 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-        type: String
+      type: String,
     },
     createdBy: {
-        type: String
+      type: String,
     },
     createdAt: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
-        type: String,
-        default: 'Large'
+      type: String,
+      default: "Large",
     },
-    toppings: []
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 module.exports = Pizza;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const CommentRoutes = require('./comment-routes');
 
+router.use('/comments', CommentRoutes);
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
